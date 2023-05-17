1bc88ce62243366f67ec7a51c5386608abb9fefa
Matthew Hoover <matthewhoover13@gmail.com>
2023-05-17 11:32:59 -0500

    Initial commit

---
+++ src/main/java/hoover/becomemon/BecomemonClient.java
@@ -0,0 +1,10 @@
+package hoover.becomemon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BecomemonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/hoover/becomemon/BecomemonDataGenerator.java
@@ -0,0 +1,11 @@
+package hoover.becomemon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BecomemonDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
