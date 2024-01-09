b70afe9c9990e399abfa582f63ccb114436385f4
Matthew Hoover <matthewhoover13@gmail.com>
2024-01-08 20:43:06 -0600

    added key bind for opening a simple gui screen

--- src/main/java/hoover/becomemon/event/KeyInputHandler.java
+++ src/main/java/hoover/becomemon/event/KeyInputHandler.java
@@ -1,5 +1,6 @@
 package hoover.becomemon.event;
 
+import hoover.becomemon.screen.TutorialScreen;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -10,14 +11,21 @@ import org.lwjgl.glfw.GLFW;
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_BECOMEMON = "key.category.becomemon";
     public static final String KEY_TEST = "key.becomemon.test";
+    public static final String KEY_TYPES = "key.becomemon.types";
 
     public static KeyBinding testKey;
+    public static KeyBinding typesKey;
 
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
            if (testKey.wasPressed()) {
                // This happens when our custom key is pressed
                client.player.sendMessage(Text.literal("Test"));
+           }
+           else if (typesKey.wasPressed()) {
+               if (client.player != null) {
+                   client.setScreenAndRender(new TutorialScreen(client.currentScreen));
+               }
            }
         });
     }
@@ -27,6 +35,12 @@ public class KeyInputHandler {
                 KEY_TEST,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_R,
+                KEY_CATEGORY_BECOMEMON
+        ));
+        typesKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_TYPES,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_B,
                 KEY_CATEGORY_BECOMEMON
         ));
 
